329133d11bd047fe8b5ee1f4f92ae3ce59ebad50
Aleksandr Sazhin <i4j5@ya.ru>
2015-10-18 22:09:35 +0300

    adding, deleting and editing of status

--- app.php
+++ app.php
@@ -16,4 +16,9 @@ $app->get('/delete/{id}', 'Http\Controllers\Orders:delete');
 
 $app->get('/login', 'Http\Controllers\Auth:login');
 
+$app->get('/settings/status', 'Http\Controllers\Status:index');
+$app->map(['GET', 'POST'], '/settings/status/create', 'Http\Controllers\Status:create');
+$app->map(['GET', 'POST'], '/settings/status/edit/{id}', 'Http\Controllers\Status:edit');
+$app->get('/settings/status/delete/{id}', 'Http\Controllers\Status:delete');
+
 return $app;
